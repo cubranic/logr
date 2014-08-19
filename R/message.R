f2a7f3f6de3ed83c379f472285b8faa3f2039557
Davor Cubranic <cubranic@stat.ubc.ca>
2014-08-19 16:03:30 -0700

    Write a help page for `format_message` (closes #4)

--- R/message.R
+++ R/message.R
@@ -7,39 +7,48 @@ LoggingMessage <- function(message, level = 'INFO', call = NULL) {
 }
 
 
-## Generic for generating the text of logging messages from its
-## argument
-##
-## @param x the object to put in the log
-## @param ... further arguments to specific methods
+#' Construct logging message text
+#'
+#' Generic for constructing the text of logging messages.
+#'
+#' These functions provide a convenient way to format logging messages
+#' based on the object being logged. For instance, data frames are
+#' formatted using \code{\link[base]{print.data.frame}}, while text
+#' objects can optionally be formatted using
+#' \code{\link[base]{strwrap}}. When formatting is delegated to these
+#' methods, the caller can control them by passing additional
+#' arguments, such as \code{width} for \code{strwrap}.
+#'
+#' @param x the object to put in the log
+#' @param appendLF (logical) should the message text have a newline
+#'                 appended at each line
+#' @param wrap (logical) should the message text first be reformatted
+#'             using `strwrap`
+#' @param ... additional arguments passed through to specific methods (see below)
+#'
+#' @keywords internal
+#' @export
 format_message <- function(x, ...) {
     UseMethod('format_message')
 }
 
 
-## The default `format_message` method simply coerces its argument to
-## character and pastes it together with no separator
-##
-## @param x the object to put in the log
-## @param appendLF (logical) should the message text have a newline
-##                 appended at each line
-## @param ... further arguments to other methods that are ignored in
-##            this function
+#' @describeIn format_message coerce the argument to character and
+#' concatenate its elements
+#'
+#' @keywords internal
+#' @export
 format_message.default <- function(x, appendLF = TRUE, ...) {
     message <- paste(as.character(x), collapse = '')
     format_message.character(message, appendLF = appendLF, wrap = FALSE)
 }
 
 
-## The `format_message` method for character vectors optionally formats
-## it by wrapping long lines using `base::strwrap`
-##
-## @param x the character vector object to put in the log
-## @param appendLF (logical) should the message text have a newline
-##                 appended at each line
-## @param wrap (logical) should the message text first be reformatted
-##             using `strwrap`
-## @param ... optional arguments passed through to `strwrap`
+#' @describeIn format_message log the character vector optionally
+#' formatting it using \code{\link[base]{strwrap}}
+#'
+#' @keywords internal
+#' @export
 format_message.character <- function(x, appendLF = TRUE, wrap = any(nchar(x) > 0.9 * getOption('width')), ...) {
     if (wrap) {
         x <- strwrap(x, ...)
@@ -51,11 +60,12 @@ format_message.character <- function(x, appendLF = TRUE, wrap = any(nchar(x) > 0
 }
 
 
-## The `format_message` method for data frames converts the object to its
-## textual representation using `base::print.data.frame`
-##
-## @param x the data frame object to put in the log
-## @param ... optional arguments passed through to `print.data.frame`
+#' @describeIn format_message log the data frame using
+#' \code{\link[base]{print.data.frame}} to construct in textual
+#' representation
+#'
+#' @keywords internal
+#' @export
 format_message.data.frame <- function(x, ...) {
     message <- NULL
     file <- textConnection('message', 'w', local = TRUE)
